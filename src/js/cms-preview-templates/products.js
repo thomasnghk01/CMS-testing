9fbd9fc02b6223d5a466185c1dd1449e26e8bde5
Thomas ng <thomasng@hk01.com>
2017-08-11 18:13:08 +0800

    no message

--- src/js/cms-preview-templates/products.js
+++ src/js/cms-preview-templates/products.js
@@ -27,18 +27,16 @@ export default class PostPreview extends React.Component {
           </div>
         </div>
       </div>
-
       <div className="bg-off-white pv4">
         <div className="ph3 mw7 center">
           <h2 className="f2 b lh-title mb2">{entry.getIn(["data", "intro", "heading"])}</h2>
           <p className="mb4 mw6">{entry.getIn(["data", "intro", "description"])}</p>
-
-          <div className="flex-ns flex-wrap mhn2-ns mb3">
-            {(entry.getIn(["data","intro", "blurbs"]) || []).map((blurb, index) => <div className="ph2-ns w-50-ns mb4" key={index}>
-              <img src={blurb.get("image") && getAsset(blurb.get("image"))} alt="" className="center db mb3" style={{width: "240px"}}/>
-              <p>{blurb.get("text")}</p>
-            </div>)}
-          </div>
+            <div className="flex-ns flex-wrap mhn2-ns mb3">
+              {(entry.getIn(["data","intro", "blurbs"]) || []).map((blurb, index) => <div className="ph2-ns w-50-ns mb4" key={index}>
+                <img src={blurb.get("image") && getAsset(blurb.get("image"))} alt="" className="center db mb3" style={{width: "240px"}}/>
+                <p>{blurb.get("text")}</p>
+              </div>)}
+            </div>
         </div>
       </div>
 
